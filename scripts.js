d13e283dfdba1072fd676df03b2499ceca15a5be
yostiq <amin.karaoui@gmail.com>
2020-02-09 23:11:13 +0200

    Kortit jakautuu pöytään

--- scripts.js
+++ scripts.js
@@ -12,8 +12,6 @@ let firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 let db = firebase.firestore();
 
-let deck_id = "";
-
 window.onclick = (event) => {
     if (event.target === document.getElementById("logonBackground")) {
         document.getElementById("logonBackground").style.display = "none";
@@ -21,47 +19,6 @@ window.onclick = (event) => {
         document.getElementById("loginBackground").style.display = "none";
     }
 };
-
-async function getDeck(amount) {
-    let url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=" + amount;
-    if (deck_id === null || deck_id === "") {
-        await fetch(url)
-            .then((response) => response.json())
-            .then((json) => {
-                deck_id = json.deck_id;
-            })
-            .catch((error) => console.log(error));
-    }
-    return deck_id;
-}
-
-async function drawCard(amount, player) {
-    console.log(deck_id);
-    let url = "https://deckofcardsapi.com/api/deck/" + deck_id + "/draw/?count=" + amount;
-
-
-    await fetch(url)
-        .then((response) => response.json())
-        .then((json) => addToHand(json, player))
-        .catch((error) => console.log(error))
-}
-
-function addToHand(json, player) {
-    console.log("addToHand: ");
-    console.log(json);
-    let url = "https://deckofcardsapi.com/api/deck/" + deck_id + "/pile/" + player + "/add/?cards=";
-
-    for (let i = 0; i < json.cards.length; i++) {
-        if (i === json.cards.length - 1) {
-            url += json.cards[i].code;
-        } else {
-            url += json.cards[i].code + ",";
-        }
-    }
-    console.log(url);
-    fetch(url)
-        .catch((error) => console.log(error));
-}
 
 function createUser() {
     let username = document.getElementById("createUsername");
@@ -110,8 +67,7 @@ function cancelButton() {
     document.getElementById("logonBackground").style.display = "none";
 }
 
-document.getElementById("drawCard").addEventListener("click", function () {
-    getDeck(1)
-        .then(() => drawCard(5, "amin"));
+document.querySelector("#play-blackjack").addEventListener("click", function () {
+    document.querySelector("#play-blackjack").remove();
+    blackjack();
 });
-
