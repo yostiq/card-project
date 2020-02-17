f13352907d8feee27d1f7224ff5d400d3eb77b1d
jopakka <jopakkamies@gmail.com>
2020-02-17 18:04:39 +0200

    secret poker solver

---
+++ poker.js
@@ -0,0 +1,92 @@
+let poker_deck = "";
+const cpuPlayers = 3;
+
+let pokerPlayers = [
+    {
+        name: "player",
+        cards: []
+    }
+];
+
+async function poker() {
+    if (poker_deck === ""){
+        const url = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+        await fetch(url)
+            .then((response) => response.json())
+            .then((json) => poker_deck = json.deck_id)
+            .catch((error) => console.log(error));
+    } else {
+        const url = "https://deckofcardsapi.com/api/deck/" + poker_deck + "/shuffle/";
+        await fetch(url)
+            .catch((error) => console.log(error));
+    }
+
+    for (let i = 0; i < cpuPlayers; i++) {
+        let cpu = {
+            name: "cpu" + (i + 1),
+            cards: []
+        };
+        pokerPlayers.push(cpu);
+    }
+
+    for (let i = 0; i < pokerPlayers.length; i++){
+        await drawCard(5, pokerPlayers[i]);
+        await getHand(pokerPlayers[i]);
+    }
+}
+
+async function drawCard(amount, player){
+    let url = "https://deckofcardsapi.com/api/deck/" + poker_deck + "/draw/?count=" + amount;
+
+    await fetch(url)
+        .then((response) => response.json())
+        .then((json) => addToHand(json, player))
+        .catch((error) => console.log(error));
+}
+
+async function addToHand(json, player) {
+    let url = "https://deckofcardsapi.com/api/deck/" + poker_deck + "/pile/" + player.name + "/add/?cards=";
+    for (let i = 0; i < json.cards.length; i++) {
+        if(json.cards[i].value === "JACK"){
+            json.cards[i].value = "11";
+        } else if(json.cards[i].value === "QUEEN"){
+            json.cards[i].value = "12";
+        } else if(json.cards[i].value === "KING"){
+            json.cards[i].value = "13";
+        } else if(json.cards[i].value === "ACE"){
+            json.cards[i].value = "14";
+        }
+
+        player.cards.push(json.cards[i]);
+
+        if (i === json.cards.length - 1) {
+            url += json.cards[i].code;
+        } else {
+            url += json.cards[i].code + ",";
+        }
+    }
+
+    await fetch(url)
+        .catch((error) => console.log(error));
+}
+
+async function getHand(player) {
+    player.cards.sort((a,b) => b.value - a.value);
+    console.log(player.cards);
+}
+
+document.querySelector("#openPoker").addEventListener("click", async function () {
+    await poker();
+});
+
+/*
+
+//FOR TEST PURPOSES ONLY!
+
+document.querySelector("#openTexas").addEventListener("click", async function () {
+    for(let i = 0; i < pokerPlayers.length; i++){
+        console.log(pokerPlayers[i].cards);
+        console.log(solve(pokerPlayers[i].cards));
+    }
+});
+*/
---
+++ pokerSolver.js
@@ -0,0 +1,173 @@
+function solve(cards) {
+    return royalFlush(cards);
+}
+
+function royalFlush(cards) {
+    if (!royalChecker(cards) || !flushChecker(cards) || !straightChecker(cards)) {
+        return straightFlush(cards);
+    }
+
+    return "ROYAL_FLUSH_" + cards[0].suit;
+}
+
+function straightFlush(cards) {
+    if (!flushChecker(cards) || !straightChecker(cards)) {
+        return fourOfAKind(cards);
+    }
+
+    return "STRAIGHT_FLUSH_" + cards[0].suit + "_" + cards[0].value;
+}
+
+function fourOfAKind(cards) {
+    let sameKinds = sameKindChecker(cards, 4);
+    if (sameKinds === false) {
+        return fullHouse(cards);
+    }
+
+    return "FOUR_OF_A_KIND_" + sameKinds;
+}
+
+function fullHouse(cards) {
+    let threeSameKinds = sameKindChecker(cards, 3);
+    if (!threeSameKinds) {
+        return flush(cards);
+    }
+
+    let secondPair = secondPairChecker(cards, threeSameKinds);
+    if (!secondPair) {
+        return flush(cards);
+    }
+
+    return "FULL_HOUSE_" + threeSameKinds + "_" + secondPair;
+}
+
+function flush(cards) {
+    if (!flushChecker(cards)) {
+        return straight(cards);
+    }
+
+    return "FLUSH_" + cards[0].suit;
+}
+
+function straight(cards) {
+    if (!straightChecker(cards)) {
+        return threeOfAKind(cards);
+    }
+
+    return "STRAIGHT_" + cards[0].value;
+}
+
+function threeOfAKind(cards) {
+    let sameKinds = sameKindChecker(cards, 3);
+    if (!sameKinds) {
+        return twoPairs(cards);
+    }
+
+    return "THREE_OF_A_KIND_" + sameKinds;
+}
+
+function twoPairs(cards) {
+    let firstPair = sameKindChecker(cards, 2);
+    if (!firstPair) {
+        return pair(cards);
+    }
+
+    let secondPair = secondPairChecker(cards, firstPair);
+    if (!secondPair) {
+        return pair(cards);
+    }
+
+    if (parseInt(secondPair) > parseInt(firstPair)) {
+        return "TWO_PAIRS_" + secondPair + "_" + firstPair;
+    }
+    return "TWO_PAIRS_" + firstPair + "_" + secondPair;
+}
+
+function pair(cards) {
+    let sameKinds = sameKindChecker(cards, 2);
+    if (!sameKinds) {
+        return highCard(cards);
+    }
+
+    return "PAIR_" + sameKinds;
+}
+
+function highCard(cards) {
+    return "HIGH_CARD_" + cards[0].value;
+}
+
+function royalChecker(cards) {
+    return cards[0].value === "14";
+}
+
+function flushChecker(cards) {
+    let suit = cards[0].suit;
+    for (let i = 1; i < cards.length; i++) {
+        if (cards[i].suit !== suit) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function straightChecker(cards) {
+    if (cards[0].value === "14" && cards[1].value !== "13") {
+        cards[0].value = "1";
+        cards.sort((a, b) => b.value - a.value);
+    }
+
+    for (let i = 0; i < cards.length - 1; i++) {
+        if (parseInt(cards[i].value) - 1 !== parseInt(cards[i + 1].value)) {
+            if (cards[cards.length - 1].value === "1") {
+                cards[cards.length - 1].value = "14";
+                cards.sort((a, b) => b.value - a.value);
+            }
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function sameKindChecker(cards, wantedAmount) {
+    let sameAmount;
+    let wantedValue;
+    for (let i = 0; i < 6 - wantedAmount; i++) {
+        sameAmount = 0;
+        wantedValue = cards[i].value;
+        for (let j = 0; j < cards.length; j++) {
+            if (cards[j].value === wantedValue) {
+                sameAmount++;
+                if (wantedAmount === sameAmount) {
+                    return wantedValue;
+                }
+            }
+        }
+    }
+
+    return false;
+}
+
+function secondPairChecker(cards, otherValue) {
+    let first = 0;
+    let wanted;
+    for (let i = 0; i < 2; i++) {
+        let amount = 0;
+        for (let j = first; j < cards.length; j++) {
+            if (cards[j].value !== otherValue && amount === 0) {
+                wanted = cards[j].value;
+                first = j + 1;
+            }
+            if (cards[j].value === wanted) {
+                amount++;
+            }
+        }
+
+        if (amount === 2) {
+            return wanted;
+        }
+    }
+
+    return false;
+}
